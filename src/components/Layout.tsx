96b720dd7140311a1ea0af7ec58f847ecddf56b8
Wes Warriner <fridgehistory@gmail.com>
2023-09-01 05:42:20 -0600

    add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import Header from './Header'
+import Footer from './Footer'
+
+interface Props {
+    children: React.ReactNode
+}
+
+const Layout: React.FunctionComponent<Props> = ( props ) => {
+    return (
+        <div className="min-h-screen flex flex-col bg-white dark:bg-gray-800">
+            <Header />
+                <main className="flex-grow container mx-auto ox-4 sm:px-6">
+                    {props.children}
+                </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
