269f66629401980ebdc1457fe8a2c96704ce3278
Wes Warriner <fridgehistory@gmail.com>
2023-09-03 14:52:31 -0700

    fix header spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,14 @@ export default function Header() {
     return (
         <header className="border-b border-gray-100">
             <div className="container mx-auto px-4 sm:px-6 py-4">
-                <p className="flex items-center space-x-1 text-black-700 dark:text-gray-300">
+                <div className="flex items-center justify-between text-black-700 dark:text-gray-300">
                     <span
                         className="font-bold text-lg tracking-light whitespace-nowrap"
                     >
                         Time Since...
                     </span>
-                </p>
-                <DarkModeToggle />
+                    <DarkModeToggle />
+                </div>
             </div>
         </header>
     )
