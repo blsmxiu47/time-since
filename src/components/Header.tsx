0e77249b2887b41c0e09f802317d56c6518fd37d
Wes Warriner <fridgehistory@gmail.com>
2023-09-03 15:23:03 -0700

    clean up logo svgs and make visual improvements to header and footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import DarkModeToggle from "./DarkModeToggle";
 
 export default function Header() {
     return (
-        <header className="border-b border-gray-100">
+        <header className="border-b dark:border-gray-300">
             <div className="container mx-auto px-4 sm:px-6 py-4">
                 <div className="flex items-center justify-between text-black-700 dark:text-gray-300">
                     <span
