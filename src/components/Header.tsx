d5ccfb73d5e02c3e27a2f1d55943d7a4ac9ce188
Wes Warriner <fridgehistory@gmail.com>
2023-09-04 18:36:36 -0700

    add fairly useless anchor to the app title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,13 @@ export default function Header() {
         <header className="border-b dark:border-gray-300">
             <div className="container mx-auto px-4 sm:px-6 py-4">
                 <div className="flex items-center justify-between text-black-700 dark:text-gray-300">
-                    <span
-                        className="font-bold text-lg tracking-light whitespace-nowrap"
-                    >
-                        Time Since...
-                    </span>
+                    <a href="#" className="focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+                        <span
+                            className="font-bold text-lg tracking-light whitespace-nowrap"
+                        >
+                            Time Since...
+                        </span>
+                    </a>
                     <DarkModeToggle />
                 </div>
             </div>
